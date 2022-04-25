020e14e6e56d55335161718269a5fb74e0f89dcd
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-04-26 01:01:29 +0900

    finished Displaying Cell Info in Collection Views and Navigation(Chapter 2)

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -31,6 +31,7 @@ class ReminderListViewController: UICollectionViewController {
         dataSource.apply(snapshot)
         
         collectionView.dataSource = dataSource
+        
     }
     
     
