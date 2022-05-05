7b074f4a2c5a26715c7d59644868ff04444292ed
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-05-06 00:40:30 +0900

    finished Using Content Views in Chapter 3.

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Hito4TK on 2022/05/05.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
